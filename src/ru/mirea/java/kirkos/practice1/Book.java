3df2336369698c3c6209984ae61ff96e59d60655
Kirill-san <kir.kositzin@yandex.ru>
2021-09-26 14:59:54 +0300

    pr1 get set

--- src/ru/mirea/java/kirkos/practice1/Book.java
+++ src/ru/mirea/java/kirkos/practice1/Book.java
@@ -1,4 +1,32 @@
 package ru.mirea.java.kirkos.practice1;
 
 public class Book {
+
+    private String author;
+    private int pages;
+    private short year;
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public void setYear(short year) {
+        this.year = year;
+    }
+
+    public String getAuthor () {
+        return author;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public short getYear() {
+        return year;
+    }
 }
---
+++ src/ru/mirea/java/kirkos/practice1/BookTester.java
@@ -0,0 +1,7 @@
+package ru.mirea.java.kirkos.practice1;
+
+public class BookTester {
+    public static void main(String[] args) {
+        System.out.println("Класс 'Книга'");
+    }
+}
