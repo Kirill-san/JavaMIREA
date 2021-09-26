73dbdefbb3fff2378d0971ccd37489621ce54bc1
Kirill-san <kir.kositzin@yandex.ru>
2021-09-26 14:37:48 +0300

    test 0 is ready

--- src/ru/mirea/java/kirkos/practice0/Class1.java
+++ src/ru/mirea/java/kirkos/practice0/Class1.java
@@ -2,7 +2,7 @@ package ru.mirea.java.kirkos.practice0;
 
 public class Class1 {
     public static void main (String[] args) {
-        //тестовая программа
+        //test
         System.out.println("Hello World");
     }
 }
