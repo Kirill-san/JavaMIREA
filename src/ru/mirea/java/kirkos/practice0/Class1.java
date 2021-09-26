270b9993c4e58dacaef8903ec346078ec1f3fbb2
Kirill-san <kir.kositzin@yandex.ru>
2021-09-26 14:19:08 +0300

    add 2x hello world

--- src/ru/mirea/java/kirkos/practice0/Class1.java
+++ src/ru/mirea/java/kirkos/practice0/Class1.java
@@ -3,6 +3,8 @@ package ru.mirea.java.kirkos.practice0;
 public class Class1 {
     public static void main (String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world");
         //лалала
+        //talala
     }
 }
