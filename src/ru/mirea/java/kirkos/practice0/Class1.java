411c7c572ea033641098d6938a8c71ae96c0f76e
Kirill-san <kir.kositzin@yandex.ru>
2021-09-26 14:20:42 +0300

    final version

--- src/ru/mirea/java/kirkos/practice0/Class1.java
+++ src/ru/mirea/java/kirkos/practice0/Class1.java
@@ -2,9 +2,7 @@ package ru.mirea.java.kirkos.practice0;
 
 public class Class1 {
     public static void main (String[] args) {
+        //тестовая программа
         System.out.println("hello world");
-        System.out.println("hello world");
-        //лалала
-        //talala
     }
 }
